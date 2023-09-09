1a8689dd01e116451bc66ee6dc8ef8e79f5a94e8
Lalo Freak <lalofreak.dev@gmail.com>
2023-09-09 01:15:04 -0300

    adding comunication between front and back actions

--- src/controllers/get-server-status.js
+++ src/controllers/get-server-status.js
@@ -1,5 +1,9 @@
+const { message } = require('../messages');
+
 const router = require('express').Router();
 
-router.get('/', (req,res) => { res.status(200).send('Server listening...')});
+router.get('/', (req,res) => { 
+  res.status(200).send({ status: true, message: message.server.connectingServer })
+});
 
 module.exports = router;
--- src/messages/index.js
+++ src/messages/index.js
@@ -1,5 +1,10 @@
 const message = {
-
+  server: {
+    connectingServer: "Conectando con el servidor...",
+    connected: 'Conectado.',
+    disconnected: 'Desconectado.',
+    error: 'Error.'
+  }
 }
 
 module.exports = { message }
